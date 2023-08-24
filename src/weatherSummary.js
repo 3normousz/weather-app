47c93511e6615d494f7b80e95f02158a13049425
Phakapon <61725943+3normousz@users.noreply.github.com>
2023-08-25 02:51:50 +0700

    Updated unit display, converting

--- src/weatherSummary.js
+++ src/weatherSummary.js
@@ -21,6 +21,26 @@ function WeatherSummary({ weatherData, dailyTempData, hourlyForeCast, unitValue 
                 return Math.floor(temp - 273.15);
         }
     }
+    function celsius_to_unit(temp, unitValue) {
+        switch (unitValue) {
+            case "0":
+                return Math.floor(temp);
+            case "1":
+                return Math.floor(temp * 9 / 5 + 32);
+            default:
+                return Math.floor(temp);
+        }
+    }
+    function unitDisplay(unitValue) {
+        switch (unitValue) {
+            case "0":
+                return "°C";
+            case "1":
+                return "°F";
+            default:
+                return "°C";
+        }
+    }
     function now_to_end(hourlyForeCast) {
         let cnt = 0;
         while (hourlyForeCast.current_weather.time != hourlyForeCast.hourly.time[cnt]) {
@@ -33,7 +53,7 @@ function WeatherSummary({ weatherData, dailyTempData, hourlyForeCast, unitValue 
         return (
             <div className="px-4 py-6">
                 <div className="font-bold text-xl mb-2">{props.timestamp.substring(11, 17)}</div>
-                <div className="text-white text-base">{props.temp}</div>
+                <div className="text-white text-base">{celsius_to_unit(props.temp, unitValue)} {unitDisplay(unitValue)}</div>
             </div>
         );
     };
@@ -58,16 +78,18 @@ function WeatherSummary({ weatherData, dailyTempData, hourlyForeCast, unitValue 
                 <h1 className='text-white text-xl font-bold'>{name}, {sys.country}</h1>
             </div>
             <div className='flex items-center justify-center'>
-                <p className='text-white text-4xl font-bold'>{kelvin_to_unit(main.temp, unitValue)}</p>
+                <p className='text-white text-4xl font-bold'>{kelvin_to_unit(main.temp, unitValue)} {unitDisplay(unitValue)}</p>
             </div>
             <div className='flex flex-row items-center justify-center'>
                 <p className='text-white text-lg'>{weather[0].description}</p>
             </div>
             <div className='flex flex-row items-center justify-center'>
-                <p className='text-white text-lg'>High: {dailyTempData.daily.temperature_2m_max[0]} Low: {dailyTempData.daily.temperature_2m_min[0]}</p>
+                <p className='text-white text-lg'>High: {Math.floor(dailyTempData.daily.temperature_2m_max[0])} {unitDisplay(unitValue)} | Low: {Math.floor(dailyTempData.daily.temperature_2m_min[0])} {unitDisplay(unitValue)}</p>
             </div>
-            <div className="flex flex-row rounded overflow-x-scroll bg-black text-white shadow-lg mt-6">
-                {componentsArray()}
+            <div className="overflow-hidden">
+                <div className="flex flex-row rounded overflow-x-auto text-white shadow-lg mt-6 bg-gray-500">
+                    {componentsArray()}
+                </div>
             </div>
         </div>
     )
