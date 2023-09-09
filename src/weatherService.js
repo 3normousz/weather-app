f69c231abef59555b45b9e68a8f01b38838a15e2
Phakapon <61725943+3normousz@users.noreply.github.com>
2023-09-10 05:27:38 +0800

    Bugs fixed, added more information cards
    - Now icon display based on time, for example, if it's 01.00 a.m., the icon will be the moon instead of the sun.
    - Hourly forecast now based on city's timezone
    - Added UV Index, Sun Rise, Feels Like, Humidity cards.
    - Correction unit display

--- src/weatherService.js
+++ src/weatherService.js
@@ -1,22 +1,21 @@
-const ROOT_URL = 'https://api.openweathermap.org/data/2.5/weather?q='
-const ROOT_DAILY_TEMP_URL = 'https://api.open-meteo.com/v1/forecast?latitude='
-const ROOT_HOURLY_FORECAST_URL = 'https://api.open-meteo.com/v1/forecast?latitude='
+const ROOT_OPENWEATHERMAP_URL = 'https://api.openweathermap.org/data/2.5/weather?q='
+const ROOT_OPENMETEO_URL = 'https://api.open-meteo.com/v1/forecast?latitude='
 const API_KEY = 'c704e95a7a33d01511028b59f66c0345'
 
 export const getWeatherData = (searchParameter) => {
-    const url = new URL(ROOT_URL + searchParameter + '&appid=' + API_KEY);
+    const url = new URL(ROOT_OPENWEATHERMAP_URL + searchParameter + '&appid=' + API_KEY);
     return fetch(url).then((res) => res.json())
 };
 
 export const getDailyTemp = (lat, lon) => {
-    const url = new URL(ROOT_DAILY_TEMP_URL + lat + '&longitude=' + lon +
-        '&daily=temperature_2m_max,temperature_2m_min&timeformat=unixtime&timezone=auto');
+    const url = new URL(ROOT_OPENMETEO_URL + lat + '&longitude=' + lon +
+        '&daily=temperature_2m_max,temperature_2m_min,sunrise,uv_index_max&timezone=auto');
     return fetch(url).then((res) => res.json())
 };
 
 export const getHourlyForeCast = (lat, lon) => {
-    const url = new URL(ROOT_DAILY_TEMP_URL + lat + '&longitude=' + lon +
-        '&hourly=temperature_2m,weathercode&current_weather=true&timeformat=unixtime&timezone=auto');
+    const url = new URL(ROOT_OPENMETEO_URL + lat + '&longitude=' + lon +
+        '&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,weathercode,is_day&current_weather=true&timezone=auto');
     return fetch(url).then((res) => res.json())
 };
 
--- src/weatherSummary.js
+++ src/weatherSummary.js
@@ -16,7 +16,7 @@ function WeatherSummary({ weatherData, dailyTempData, hourlyForeCast, unitValue 
             case 0:
                 return Math.floor(temp - 273.15);
             case 1:
-                return Math.floor((temp - 273.15) * 9 / 5 + 32)
+                return Math.floor((temp - 273.15) * 9 / 5 + 32);
             default:
                 return Math.floor(temp - 273.15);
         }
@@ -26,7 +26,7 @@ function WeatherSummary({ weatherData, dailyTempData, hourlyForeCast, unitValue 
             case 0:
                 return Math.floor(temp);
             case 1:
-                return Math.floor(temp * 9 / 5 + 32);
+                return Math.floor(((temp * 9) / 5) + 32);
             default:
                 return Math.floor(temp);
         }
@@ -42,34 +42,9 @@ function WeatherSummary({ weatherData, dailyTempData, hourlyForeCast, unitValue 
         }
     }
 
-    function getTimeStamp(unix_timestamp) {
-        var date = new Date(unix_timestamp * 1000);
-
-        var hours = date.getHours();
-
-        var minutes = "0" + date.getMinutes();
-
-        var formattedTime = hours + ':' + minutes;
-
-        return [formattedTime, date.getDate(), date.getMonth() + 1];
-    }
-
     function getCurrentTimeStampIndex(hourlyForeCast) {
-        let left = 0;
-        let right = 26;
-        let mid = 0;
-
-        while (left <= right) {
-            mid = Math.floor(left + (right - left) / 2);
-            if (hourlyForeCast.current_weather.time == hourlyForeCast.hourly.time[mid]) {
-                return mid;
-            }
-            if (hourlyForeCast.hourly.time[mid] < hourlyForeCast.current_weather.time) {
-                left = mid + 1;
-            }
-            else {
-                right = mid - 1;
-            }
+        for (let i = 0; i <= 24; i++) {
+            if (hourlyForeCast.hourly.time[i] === hourlyForeCast.current_weather.time) return i;
         }
     };
 
@@ -77,8 +52,8 @@ function WeatherSummary({ weatherData, dailyTempData, hourlyForeCast, unitValue 
         let dateElement = null;
 
 
-        if (getTimeStamp(props.timestamp)[0] === "0:00") {
-            dateElement = <div className="font-bold text-l">{getTimeStamp(props.timestamp)[1]}/{getTimeStamp(props.timestamp)[2]}</div>;
+        if (props.timestamp.substring(11, 16) === "00:00") {
+            dateElement = <div className="font-bold text-l">{props.timestamp.substring(5, 10)}</div>;
         } else {
             dateElement = <div className="font-bold text-l">&nbsp;</div>;
         }
@@ -87,10 +62,10 @@ function WeatherSummary({ weatherData, dailyTempData, hourlyForeCast, unitValue 
         return (
             <div className="px-6 py-2">
                 {dateElement}
-                <div className="font-bold text-xl">{getTimeStamp(props.timestamp)[0]}</div>
-                <img alt="weather-icon" src={`icons/${props.weatherIconID}.png`} />
+                <div className="font-bold text-xl">{props.timestamp.substring(11, 16)}</div>
+                <img alt="weather-icon" src={`icons/${props.weatherIconID}${props.weatherIconIsDay}.png`} />
                 <div className="text-white text-base mt-1">
-                    {celsius_to_unit(props.temp, unitValue)} {unitDisplay(unitValue)}
+                    {celsius_to_unit(Math.floor(props.temp), unitValue)} {unitDisplay(unitValue)}
                 </div>
             </div>
         );
@@ -101,62 +76,104 @@ function WeatherSummary({ weatherData, dailyTempData, hourlyForeCast, unitValue 
         let arr = [];
         for (let i = getCurrentTimeStampIndex(hourlyForeCast); i <= 167; i++) {
             let hourly = hourlyForeCast.hourly;
-            arr.push(<HourlyForeCastComponent key={i} timestamp={hourly.time[i]}
-                temp={hourly.temperature_2m[i]}
-                weatherIconID={convertWeatherIcon(hourly.weathercode[i])}
-            />);
+            arr.push(
+                <HourlyForeCastComponent
+                    key={i}
+                    timestamp={hourly.time[i]}
+                    temp={hourly.temperature_2m[i]}
+                    weatherIconID={convertWeatherIcon(hourly.weathercode[i])}
+                    weatherIconIsDay={checkHourlyForeCastIsDay(hourly.is_day[i])}
+                />);
         }
         return arr;
     };
 
     function convertWeatherIcon(api1Code) {
         const api1ToApi2Mapping = {
-            "0": "01d",  // Clear sky
-            "1": "02d", // Few clouds
-            "2": "02d",
-            "3": "02d",
-            "45": "50d", // Fog
-            "48": "50d",
-            "51": "09d", // Shower rain
-            "53": "09d",
-            "55": "09d",
-            "61": "10d", // Rain
-            "63": "10d",
-            "65": "10d",
-            "80": "09d", // Shower rain
-            "81": "09d",
-            "82": "09d",
-            "95": "11d", // Thunderstorm
-            "96": "11d",
-            "99": "11d",
+            "0": "01",  // Clear sky
+            "1": "02", // Few clouds
+            "2": "02",
+            "3": "02",
+            "45": "50", // Fog
+            "48": "50",
+            "51": "09", // Shower rain
+            "53": "09",
+            "55": "09",
+            "61": "10", // Rain
+            "63": "10",
+            "65": "10",
+            "80": "09", // Shower rain
+            "81": "09",
+            "82": "09",
+            "95": "11", // Thunderstorm
+            "96": "11",
+            "99": "11",
         };
         return api1ToApi2Mapping[api1Code];
     };
 
+    function checkHourlyForeCastIsDay(hourlyForeCast) {
+        if (hourlyForeCast === 0) return "n";
+        else return "d";
+    }
+
     return (
         <div>
-            <div className='flex items-center justify-center mt-10'>
+            <div className='flex items-center justify-center mt-32'>
                 <img alt="weather-icon" src={`icons/${weather[0].icon}.png`} />
             </div>
 
-            <div className='flex items-center justify-center'>
+            <div className='flex items-center justify-center mt-8'>
                 <h1 className='text-white text-xl font-bold'>{name}, {sys.country}</h1>
             </div>
-            <div className='flex items-center justify-center'>
+            <div className='flex items-center justify-center mt-4'>
                 <p className='text-white text-4xl font-bold'>{kelvin_to_unit(main.temp, unitValue)} {unitDisplay(unitValue)}</p>
             </div>
             <div className='flex flex-row items-center justify-center'>
                 <p className='text-white text-lg'>{weather[0].description}</p>
             </div>
-            <div className='flex flex-row items-center justify-center'>
+            <div className='flex flex-row items-center justify-center mt-4'>
                 <p className='text-white text-lg'>
-                    High: {Math.floor(dailyTempData.daily.temperature_2m_max[0])} {unitDisplay(unitValue)} |
-                    Low: {Math.floor(dailyTempData.daily.temperature_2m_min[0])} {unitDisplay(unitValue)}
+                    High: {celsius_to_unit(Math.floor(dailyTempData.daily.temperature_2m_max[0]), unitValue)} {unitDisplay(unitValue)} |
+                    Low: {celsius_to_unit(Math.floor(dailyTempData.daily.temperature_2m_min[0]), unitValue)} {unitDisplay(unitValue)}
                 </p>
             </div>
-            <div className="overflow-hidden">
-                <div className="flex flex-row rounded overflow-x-auto text-white shadow-lg mt-6 bg-gray-500">
-                    {hourlyForeCastComponentCreation()}
+            <div className="flex flex-col">
+                <div className="overflow-hidden">
+                    <div className="flex flex-row rounded overflow-x-auto text-white shadow-lg mt-24 bg-gray-500">
+                        {hourlyForeCastComponentCreation()}
+                    </div>
+                </div>
+                <div className="flex flex-wrap mt-36 text-white">
+                    <div className="lg:flex lg:flex-row w-full">
+                        <div className="bg-gray-500 w-1/2 rounded-tl">
+                            <div className="mb-8 mt-8">
+                                <div className="font-bold text-xl mb-2">UV Index</div>
+                                <p className="text-base">{dailyTempData.daily.uv_index_max[0]}</p>
+                            </div>
+                        </div>
+                        <div className="bg-gray-500 w-1/2 rounded-tr">
+                            <div className="mb-8 mt-8">
+                                <div className="font-bold text-xl mb-2">Sun Rise</div>
+                                <p className="text-base">{dailyTempData.daily.sunrise[0].substring(11, 16)}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="lg:flex lg:flex-row w-full">
+                        <div className="bg-gray-500 w-1/2 rounded-bl">
+                            <div className="mb-8">
+                                <div className="font-bold text-xl mb-2">Feels Like</div>
+                                <p className="text-base">{celsius_to_unit(hourlyForeCast.hourly.apparent_temperature[getCurrentTimeStampIndex(hourlyForeCast)], unitValue)} {unitDisplay(unitValue)}</p>
+                            </div>
+                        </div>
+
+                        <div className="bg-gray-500 w-1/2 rounded-br">
+                            <div className="mb-8">
+                                <div className="font-bold text-xl mb-2">Humidity</div>
+                                <p className="text-base">{hourlyForeCast.hourly.relativehumidity_2m[getCurrentTimeStampIndex(hourlyForeCast)]}%</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
